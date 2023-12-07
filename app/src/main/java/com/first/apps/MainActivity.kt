5b47dec74e3ba8bdcb41208f9289a7cbc2cde907
Dilip Boss <dilip.web7@gmail.com>
2023-12-07 12:44:05 +0530

    Initial commit

---
+++ app/src/main/java/com/first/apps/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.first.apps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_layout)
+        Log.i("Testing","Check where it printed")
+
+
+    }
+}
